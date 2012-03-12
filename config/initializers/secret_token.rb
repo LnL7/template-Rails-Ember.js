35c946ddefe4b62d884a97f10abe41eb62c4f7ae
hBook <daiderd@hotmail.com>
2012-03-12 18:42:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+REmber1::Application.config.secret_token = '9b1e958328d632bc64966893314f64685718766cb49fe8b27adcaa7e0e6611374c8c57a024e42752dd200a2dec5e3bdf19844b401502abb2fe72e52577d655bc'
